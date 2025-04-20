a67786c5f09bed847bef2ace481d37611cb334be
Luiza Amorim <luizapriv@gmail.com>
2025-04-20 20:04:04 -0300

    primeiro commit

---
+++ Produto.cs
@@ -0,0 +1,106 @@
+﻿public class Produto
+{
+    public string Nome { get; set; }
+    public string Marca { get; set; }
+    public string Cor { get; set; }
+    public int Id { get; set; }
+    public double Preco { get; set; }
+
+
+
+    public Produto()
+    {
+
+    }
+
+    public string ToString()
+    {
+        return $"Nome: {Nome}, Marca: {Marca}, Cor: {Cor}, Id: {Id}, Preco: {Preco} \n";
+    }
+
+
+    public void CadastrarProduto()
+    {
+        Console.WriteLine("Qual produto deseja cadastrar?: ");
+        Nome = Console.ReadLine();
+        Console.WriteLine("Qual a marca do produto?: ");
+        Marca = Console.ReadLine();
+        Console.WriteLine("Qual a cor do produto?: ");
+        Cor = Console.ReadLine();
+        Console.WriteLine("Qual o id do produto?: ");
+        Id = int.Parse(Console.ReadLine());
+        Console.WriteLine("Qual o preco do produto?: ");
+        Preco = double.Parse(Console.ReadLine());
+        Console.WriteLine(" \n Cadastrado com sucesso! \n");
+
+
+    }
+
+    public void ListarProdutos(List<Produto> produtos)
+    {
+        Console.WriteLine(" \nLista de produtos: \n");
+
+        foreach (var produto in produtos)
+        {
+            Console.WriteLine(produto.ToString());
+
+        }
+
+        if (produtos.Count == 0)
+        {
+            Console.WriteLine(" \nNenhum produto cadastrado!");
+        }
+
+    }
+    public void BuscarProduto(List<Produto> produtos)
+    {
+        Console.Write(" \nDigite o id do produto que deseja buscar: ");
+
+        int id = int.Parse(Console.ReadLine());
+
+        bool achou = false;
+
+        foreach (var produto in produtos)
+        {
+            if (produto.Id == id)
+            {
+
+                Console.WriteLine(produto.ToString());
+                achou = true;
+            }
+        }
+
+        if (!achou)
+        {
+            Console.WriteLine(" \n Produto não encontrado!");
+        }
+
+
+
+    }
+
+    public Produto RemoverProduto(List<Produto> produtos)
+    {
+        Console.Write(" \nDigite o id do produto que deseja remover: ");
+        int id = int.Parse(Console.ReadLine());
+
+
+
+        foreach (var produto in produtos)
+        {
+            if (produto.Id == id)
+            {
+
+                return produto;
+
+            }
+
+        }
+
+        return null;
+
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿namespace SistemaProdutos
+{
+    internal class Program
+    {
+        static List<Produto> produtos = new List<Produto>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------- Seja bem vindo ao sistema de cadastro de produtos --------\n");
+
+
+            int opcao = 0;
+
+
+            do
+
+            {
+
+                Console.WriteLine("MENU");
+                Console.WriteLine("1 - Cadastrar Produto");
+                Console.WriteLine("2 - Listar Produtos");
+                Console.WriteLine("3 - Buscar Produto");
+                Console.WriteLine("4 - Remover Produto");
+                Console.WriteLine("5 - Sair \n");
+
+                Console.Write("Digite sua opcao: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                Produto p1 = new Produto();
+
+                switch (opcao)
+                {
+                    case 1:
+                        CadastrarProduto(p1);
+                        break;
+                    case 2:
+                        ListarProdutos(p1);
+                        break;
+                    case 3:
+                        BuscarProduto(p1);
+                        break;
+                    case 4:
+                        RemoverProduto(p1);
+                        break;
+                    case 5:
+                        Console.WriteLine("Saindo do sistema...");
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida");
+                        break;
+
+                }
+
+            } while (opcao != 5);
+
+
+
+
+        }
+
+        static void CadastrarProduto(Produto p1)
+        {
+
+            p1.CadastrarProduto();
+
+            produtos.Add(p1);
+        }
+
+        static void ListarProdutos(Produto p1)
+        {
+
+            p1.ListarProdutos(produtos);
+
+        }
+
+        static void BuscarProduto(Produto p1)
+        {
+            p1.BuscarProduto(produtos);
+        }
+
+        static void RemoverProduto(Produto p1)
+        {
+
+            Produto resp = p1.RemoverProduto(produtos);
+
+            if (resp == null)
+            {
+                Console.WriteLine("Produto não encontrado");
+            }
+            else
+            {
+                produtos.Remove(resp);
+                Console.WriteLine("Produto removido com sucesso \n");
+            }
+
+
+        }
+    }
+}
+
